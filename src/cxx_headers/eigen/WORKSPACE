347ebea52f2ffd876635f66614547e421be03455
Keno Fischer <keno@juliacomputing.com>
2018-04-02 18:32:09 -0400

    Fix vendoring of Eigen

---
+++ src/cxx_headers/eigen/WORKSPACE
@@ -0,0 +1,2 @@
+# DO NOT EDIT: automatically generated WORKSPACE file for tf_http_archive rule
+workspace(name = "eigen_archive")
--- src/cxx_headers/vendor_headers.sh
+++ src/cxx_headers/vendor_headers.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-rsync -avm --include='*.h' -f 'hide,! */' ~/tensorflow/ tensorflow/
+rsync -avm --include='*.h' -f 'hide,! */' ~/tensorflow/ tensorflow
+rsync -avm --include='*' -f 'hide,! */' ~/tensorflow/third_party/eigen3/ tensorflow/third_party/eigen3
 rsync -avm --include='*.h' -f 'hide,! */' ~/.cache/bazel/_bazel_keno/194f131dbf4e1f471c25958d40967b3c/external/protobuf_archive/src/ proto/
 rsync -avm --include='*.h' -f 'hide,! */' ~/.cache/bazel/_bazel_keno/194f131dbf4e1f471c25958d40967b3c/execroot/org_tensorflow/bazel-out/host/genfiles/ genfiles/
 rsync -avm --include='*.h' -f 'hide,! */' ~/.cache/bazel/_bazel_keno/194f131dbf4e1f471c25958d40967b3c/execroot/org_tensorflow/bazel-out/k8-py3-opt/genfiles/ genfiles/
-rsync -avm --include='*.h' -f 'hide,! */' ~/.cache/bazel/_bazel_keno/194f131dbf4e1f471c25958d40967b3c/external/eigen_archive eigen/
+rsync -avm --include='*' -f 'hide,! */' ~/.cache/bazel/_bazel_keno/194f131dbf4e1f471c25958d40967b3c/external/eigen_archive/ eigen/
