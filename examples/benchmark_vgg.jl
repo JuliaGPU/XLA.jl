71a04ad89a7b7a69a7006a4ef3127563f0057828
Elliot Saba <staticfloat@gmail.com>
2018-10-28 11:26:13 -0400

    Even better benchmarking script

--- examples/benchmark_vgg.jl
+++ examples/benchmark_vgg.jl
@@ -1,14 +1,54 @@
 # Invoke this as something like `julia --project=.. benchmark_vgg.jl`
-# It is also helpful to have xrt_server already running, which you can find in `TensorFlow.jl/deps/downloads/bin`.
-# I like to do something like `CUDA_VISIBLE_DEVICES=2 ~/.julia/dev/TensorFlow/deps/downloads/bin/xrt_server`
+# To run on CPU, have an `xrt_server` already running, I like to have mine bind to the default port 8470, and I run it on localhost.
+# To run on GPU, have an `xrt_server` already running on a machine with GPUs.  I use the same `xrt_server` binary as the one I use
+# for CPU work, and I also start it with only one CUDA device available so that it doesn't immediately grab all the memory on all
+# GPUs available, as tensorflow is wont to do.  Example: `CUDA_VISIBLE_DEVICES=2 ~/.julia/dev/TensorFlow/deps/downloads/bin/xrt_server`
+# To run on TPU, open up an SSH tunnel to a GCE instance that is on the same subnet as your TPU cloud instance, bouncing off of that
+# server to access the TPU device from outside of Google's network.  Example: `ssh -L 8471:<tpu_internal_ip>:8470 <gce_external_ip>`
 using XLA, TensorFlow
 
 # For now. Media.jl has a bug
 pop!(Base.Multimedia.displays)
 
-# Connect to the TensorFlow distributed session (In this case, assumed to be xrt_server)
-sess = Session(Graph(); target="grpc://localhost:8470")
+# Connect to the TensorFlow distributed sessions (one for CPU, one for GPU, one for TPU)
+cpu_sess = Session(Graph(); target="grpc://localhost:8470")
+gpu_sess = cpu_sess
+tpu_sess = Session(Graph(); target="grpc://localhost:8471")
 
+# Try to get the associated device with each session; if one doesn't respond within 2 seconds, complain
+# because we most likely have a missing xrt_server or TPU or something.
+function wait_for_task_finish(t::Task, timeout::Float64)
+    t_start = time()
+    while !istaskdone(t)
+        sleep(0.001)
+        if time() - t_start > timeout
+            return false
+        end
+    end
+    return true
+end
+function get_device(sess, device_type)
+    device = nothing
+    t = @async begin
+        devices = collect(TensorFlow.DeviceList(sess))
+        device = first(filter(x -> x.device_type == device_type, devices))
+    end
+    if !wait_for_task_finish(t, 2.0)
+        return nothing
+    end
+    inc_number(x::Number) = x + 1
+    inc_number(x) = x
+    function fixup_device(d::TensorFlow.Device)
+        d.parts[:] .= [TensorFlow.DevicePart(p.kind, inc_number(p.index)) for p in d.parts]
+        return d
+    end
+    fixup_device(d) = d
+    return fixup_device(TensorFlow.Device(device.name))
+end
+    
+cpu_device = get_device(cpu_sess, "XLA_CPU")
+gpu_device = get_device(gpu_sess, "XLA_GPU")
+tpu_device = get_device(tpu_sess, "TPU")
 
 using Flux, Metalhead, BenchmarkTools, JLD2, Printf
 
@@ -67,46 +107,36 @@ end
 
 model = prepare_model(Metalhead.VGG19().layers)
 
-# We are going to precompile for all batch sizes we are interested in
-CPU_device = "/job:job/replica:1/task:1/device:XLA_CPU:1"
-GPU_device = "/job:job/replica:1/task:1/device:XLA_GPU:1"
-batch_sizes = (1, 2, 4, 8, 16, 32, 64)
-
-@info("Precompiling CPU and GPU models for all batch sizes:")
-compiled_models = Dict("CPU" => Dict(), "GPU" => Dict())
-t_start = time()
-for batch_size in batch_sizes
-    print("$(batch_size) ")
-    compiled_models["CPU"][batch_size] = compile_model_for_device(model, sess, CPU_device, batch_size)
-    compiled_models["GPU"][batch_size] = compile_model_for_device(model, sess, GPU_device, batch_size)
-end
-t_compiling = time() - t_start
-println()
-@info(@sprintf("Precompilation finished in %.2fs", t_compiling))
-
 # Allow benchmarking to go up to 60 seconds for each execution, so that we get
 # good statistics for each call
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 120
 BenchmarkTools.DEFAULT_PARAMETERS.samples = 10
+results = Dict()
 
-results = Dict("CPU" => Dict(), "GPU" => Dict())
-for batch_size in batch_sizes
+for batch_size in (1, 2, 4, 8, 16, 32, 64)
     x = randn(Float32, 224, 224, 3, batch_size)
 
-    # Compile model for this particular batch size
-    @info("[$batch_size CPU] Executing...")
-    results["CPU"][batch_size] = @benchmark run($(compiled_models["CPU"][batch_size]), $x)
-    display(results["CPU"][batch_size])
-    println()
-    
-    @info("[$batch_size GPU] Executing...")
-    results["GPU"][batch_size] = @benchmark run($(compiled_models["GPU"][batch_size]), $x)
-    display(results["GPU"][batch_size])
-    println()
+    for (device, sess) in [(cpu_device, cpu_sess), (gpu_device, gpu_sess), (tpu_device, tpu_sess)]
+        # Skip nonexistent devices
+        if device == nothing
+            continue
+        end
+        device_type = device.parts[end].kind[2]
 
-    # Save partial results
-    jldopen("benchmark_results.jld2", "w") do f
-        f["results"] = results
+        if !haskey(results, device_type)
+            results[device_type] = Dict()
+        end
+
+        @info "[$(device_type) $(batch_size)] Compiling..."
+        compiled_model = compile_model_for_device(model, sess, device, batch_size)
+    
+        @info("[$(device_type) $(batch_size)] Executing...")
+        results[device_type][batch_size] = @benchmark run($compiled_model, $x)
+    
+        # Save partial results
+        jldopen("benchmark_results.jld2", "w") do f
+            f["results"] = results
+        end
     end
 end
 
