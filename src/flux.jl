015256bf97af1dbddc1ab90e325488de88428ef3
Keno Fischer <keno@juliacomputing.com>
2019-02-05 18:35:19 -0500

    Bug fix - we may not always want Float32

--- src/flux.jl
+++ src/flux.jl
@@ -1,8 +1,8 @@
 using Flux
 
 function make_onehot(::Type{T}, ::Val{alphabet_size}, labels::AbstractVector{S}) where {T, S, alphabet_size}
-    bcast_labels = convert(XRTArray{Float32}, HloBroadcast((1,), (alphabet_size, length(labels)))(labels))
-    iota = convert(XRTArray{Float32}, HloBroadcast((0,), (alphabet_size, length(labels)))(XRTArray(UInt32(0):UInt32(alphabet_size-1))))
+    bcast_labels = convert(XRTArray{T}, HloBroadcast((1,), (alphabet_size, length(labels)))(labels))
+    iota = convert(XRTArray{T}, HloBroadcast((0,), (alphabet_size, length(labels)))(XRTArray(UInt32(0):UInt32(alphabet_size-1))))
     T.(iota .== bcast_labels)
 end
 @Zygote.nograd make_onehot
