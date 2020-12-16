3ed0a9ccf6b70e98326db848af1ff211bafd0883
Tim Besard <tim@juliacomputing.com>
2020-12-16 13:19:00 +0000

    Expose TpuStatus_Ok and allow creation from a handle.

--- lib/tpu/status.jl
+++ lib/tpu/status.jl
@@ -1,4 +1,4 @@
-export TpuStatus, with_status
+export TpuStatus, with_status, is_ok
 
 mutable struct TpuStatus
     handle::Ptr{TF_Status}
@@ -7,21 +7,31 @@ mutable struct TpuStatus
         handle = TpuStatus_New()
         status = new(handle)
         finalizer(TpuStatus_Free, status)
+        return status
     end
 
     function TpuStatus(code, msg)
         handle = TpuStatus_Create(code, msg)
         status = new(handle)
         finalizer(TpuStatus_Free, status)
+        return status
+    end
+
+    function TpuStatus(handle::Ptr{TF_Status}, managed=true)
+        status = new(handle)
+        managed && finalizer(TpuStatus_Free, status)
+        return status
     end
 end
 
 Base.unsafe_convert(::Type{Ptr{TF_Status}}, x::TpuStatus) = x.handle
 
+is_ok(status::TpuStatus) = TpuStatus_Ok(status)
+
 function with_status(f)
     status = TpuStatus()
     rv = f(status)
-    if !TpuStatus_Ok(status)
+    if !is_ok(status)
         throw(status)
     end
     rv
@@ -29,7 +39,7 @@ end
 
 function Base.show(io::IO, status::TpuStatus)
     print(io, "TpuStatus(")
-    if TpuStatus_Ok(status)
+    if is_ok(status)
         print(io, "OK)")
     else
         print(io, "not OK")
